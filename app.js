f087d7a48f5b5bd36b3a342bc3bfd7f7e6df8596
Jorge Rivera <jrivera@VESSELARG.local>
2022-12-27 15:01:11 -0300

    working with js

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
 // console.log("app.js connected");
 
+
+let contactBtn = document.getElementById("contactButton");
+
+contactBtn.addEventListener("mouseenter", verde);
+contactBtn.addEventListener("mouseleave", transparente);
+
+function verde (){
+    contactBtn.style.backgroundColor = "green";
+    contactBtn.style.color = "white";
+    contactBtn.style.fontWeight= "bold";
+
+    
+}
+function transparente(){
+    contactBtn.style.backgroundColor = "transparent";
+    contactBtn.style.color = "initial";
+    contactBtn.style.fontWeight= "initial";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // let mainTitle = document.getElementById("mainTitle");
 
 
