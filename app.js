d99ebbc224ef34ef4239d8c19b1193004234efbd
Jorge Rivera <jrivera@VESSELARG.local>
2022-12-27 09:58:46 -0300

    working with js

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log("app.js connected");
+
+let mainTitle = document.getElementById("mainTitle");
+
+
+
+mainTitle.addEventListener("mouseover", rojo);
+mainTitle.addEventListener("mouseout", verde);
+
+
+
+
+function verde() {
+  mainTitle.style.color = "green";
+}
+function rojo() {
+  mainTitle.style.color = "red";
+}
